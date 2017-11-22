0e59d4af57a72b2a38a27c9085ffc5362463447b
Kathy Hudson <khitx@icloud.com>
2017-11-22 23:23:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber.length = 0) {
+  if (cardNumber === undefined) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
   else {
